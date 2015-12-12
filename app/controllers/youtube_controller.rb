c2240d8e1fd253b933fd99b34f7f6eb80aa52e5b
Jason Zhang <jason@yozio.com>
2015-12-11 22:38:04 -0800

    update YoutubeController

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,10 +1,20 @@
 class YoutubeController < ApplicationController
-  # GET http://localhost:1557/youtube/download_to_dropbox?url=https://www.youtube.com/watch?v=MNrZKiamgIw
+
+  before_filter :authenticate
+
+  # GET http://localhost:3001/youtube/download_to_dropbox?url=https://www.youtube.com/watch?v=MNrZKiamgIw
   def download_to_dropbox
     if params[:url].present?
       render :text => YoutubeDownloader.download_to_dropbox(params[:url])
     else
       render :status => 404
+    end
+  end
+  private
+
+  def authenticate
+    if params[:service_key] != Constant::SERVICE_KEY
+      render :text => 'Unauthorized user'
     end
   end
 end
