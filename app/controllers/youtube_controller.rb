de80b0c1b4f57a656a23ab26b665846b3040ce9d
Jason Zhang <jason@yozio.com>
2015-12-11 23:28:57 -0800

    update controoler

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,11 +1,13 @@
 class YoutubeController < ApplicationController
 
-  before_filter :authenticate
+  # before_filter :authenticate
 
   # GET http://localhost:3001/youtube/download_to_dropbox?url=https://www.youtube.com/watch?v=MNrZKiamgIw
   def download_to_dropbox
     if params[:url].present?
-      render :text => YoutubeDownloader.download_to_dropbox(params[:url])
+      # YoutubeDownloader.download_to_dropbox(params[:url])
+      @job_id = YoutubeDownloadWorker.create
+      render :text => @job_id
     else
       render :status => 404
     end
