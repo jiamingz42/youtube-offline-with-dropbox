c99090393e49f0342b0f73b8edd5f6bcaeec2513
Jason Zhang <jason@yozio.com>
2015-12-13 17:54:38 -0800

    fix typo

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,9 @@
 # Setup the Redis server URL
-
 if ENV["REDISCLOUD_URL"]
   $redis = Resque.redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 else
-  $reids = Resque.redis = Redis.new
+  $redis = Resque.redis = Redis.new
 end
 
 # Setup the status expiration time
-
 Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60) # 24hrs in seconds
