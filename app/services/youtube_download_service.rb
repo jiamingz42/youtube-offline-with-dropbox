1c2c81cc615f160a4200801fd2e824e28f7a115f
Jason Zhang <jason@yozio.com>
2015-12-13 20:15:50 -0800

    update YoutubeDownloadService

--- app/services/youtube_download_service.rb
+++ app/services/youtube_download_service.rb
@@ -10,16 +10,32 @@ class YoutubeDownloadService
       @youtube_video_url
     end
 
+    # @todo restart failed job
     def call
       @job_id = cache.get_or_set_if_not_existed("YoutubeDownloadService:#{youtube_video_url}", expire_after: 60*60*24) do
         YoutubeDownloadWorker.create(youtube_video_url: youtube_video_url)
       end
-      @status = Resque::Plugins::Status::Hash.get(@job_id) || {}
+      job_status
     end
 
     def cache
       Cache
     end
     private :cache
+
+    def dropbox_service
+      @dropbox_service ||= DropboxService.new(ENV['DROPBOX_ACCESS_TOKEN'])
+    end
+    private :dropbox_service
+
+    def job_status
+      status = Resque::Plugins::Status::Hash.get(@job_id) || {}
+      dropbox_save_url_job = status.fetch('dropbox_save_url_job',{})
+      if dropbox_save_url_job['status'] != 'COMPLETE' && dropbox_save_url_job['job'].present?
+        dropbox_save_url_job.merge!(dropbox_service.save_url_job(dropbox_save_url_job['job']))
+      end
+      status
+    end
+    private :job_status
 
 end
