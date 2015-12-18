ec0f7fe613b62a0c9cb3607f65c60d1621725b19
Jason Zhang <jason@yozio.com>
2015-12-17 23:13:54 -0800

    move authentication to application controller

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,7 +1,5 @@
 class YoutubeController < ApplicationController
 
-  before_filter :authenticate
-
   def download_to_dropbox
     if params[:url].present?
       uri = URI(params[:url])
@@ -15,13 +13,6 @@ class YoutubeController < ApplicationController
       end
     else
       render :status => 404
-    end
-  end
-  private
-
-  def authenticate
-    if params[:service_key] != ENV['MY_SERVICE_KEY']
-      render :text => 'Unauthorized user'
     end
   end
 end
