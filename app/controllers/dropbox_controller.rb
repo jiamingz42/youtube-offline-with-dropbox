34b216943fdfa18f27ee649d0107f1e82761bd68
Jason Zhang <jason@yozio.com>
2015-12-17 23:22:50 -0800

    update the save_url action

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -1,4 +1,10 @@
 class DropboxController < ApplicationController
   def save_url
+    if [:url, :path].all? { |f| params[f].present? }
+      @dropbox_service = DropboxService.new(ENV['DROPBOX_ACCESS_TOKEN'])
+      render :json => @dropbox_service.save_url(params[:path], params[:url])
+    else
+      render :status => 500
+    end
   end
 end
