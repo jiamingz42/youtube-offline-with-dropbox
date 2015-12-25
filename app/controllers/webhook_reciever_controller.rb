e754fe94aca9dea7e45eae7e148df2a16839ffcb
Jason Zhang <jason@yozio.com>
2015-12-24 23:57:05 -0800

    parse url

--- app/controllers/webhook_reciever_controller.rb
+++ app/controllers/webhook_reciever_controller.rb
@@ -5,7 +5,23 @@ class WebhookRecieverController < ApplicationController
   end
 
   def email
-    ap params
-    render :json => params
+    sender  = params['headers']['Sender']
+    subject = params['headers']['Subject']
+    body    = params['plain']
+
+    if sender == 'benjamin19890721@gmail.com'
+      # find the URL and expand the it
+      regex = /(http:\/\/youtu\.be\/.*)/
+      match = regex.match(body)
+      if match.nil?
+        render :status => 500
+      else
+        youtube_short_url = match[0]
+        youtube_long_url = ExpandUrlService.lengthen(youtube_short_url)
+        render :text => youtube_long_url
+      end
+    else
+      render :status => 500
+    end
   end
 end
