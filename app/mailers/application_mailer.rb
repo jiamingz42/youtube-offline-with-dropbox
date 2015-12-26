e82925324ea46167c4747a8c8ae34ce42a1ab814
Jason Zhang <jason@yozio.com>
2015-12-26 00:44:58 -0800

    change attachments to attachments.inline

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,7 +8,7 @@ class ApplicationMailer < ActionMailer::Base
   def send_youtube_digest_to_evernote(options = {})
     @youtube_vidoe = options[:youtube_vidoe]
     uri = URI(@youtube_vidoe.thumbnails.first['url'].gsub('https', 'http'))
-    attachments['thumbnails'] = Net::HTTP.get(uri)
+    attachments.inline['thumbnails'] = Net::HTTP.get(uri)
     mail(to: 'benjamin19890721+forward2evernote@gmail.com, benjamin19890721.64fd093@m.evernote.com', subject: "#{options[:subject]} @Youtube #Youtube")
   end
 
