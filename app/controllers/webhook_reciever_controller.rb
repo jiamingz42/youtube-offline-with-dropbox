0bd7f137b88b8a379992a40499be0ca1cc4fceea
Jason Zhang <jason@yozio.com>
2015-12-27 20:02:15 -0800

    check sender whether match benjamin19890721@gmail.com

--- app/controllers/webhook_reciever_controller.rb
+++ app/controllers/webhook_reciever_controller.rb
@@ -8,30 +8,28 @@ class WebhookRecieverController < ApplicationController
 
   def email
     message = CloudMailinMessage.create_from_params(params)
-    body = message.plain_body
 
-    puts message.sender.match(/benjamin19890721@gmail.com/)
-    # if sender == 'benjamin19890721@gmail.com'
-      # find the URL and expand the it
-      regex = /(http[s]?:\/\/youtu\.be\/.*)/
-      match = regex.match(body)
-      if match.nil?
-        Rails.logger.debug!('No Match', context: binding)
-        render :text => 'No Match'
-      else
-        youtube_short_url = match[0]
-        youtube_long_url = ExpandUrlService.lengthen(youtube_short_url)
-        youtube_vidoe = YoutubeVideo.new(youtube_long_url)
-        ApplicationMailer.send_youtube_digest_to_evernote({
-          subject: 'Youtube Video URL',
-          youtube_vidoe: youtube_vidoe }
-          ).deliver_now
-        Rails.logger.debug!('OK', context: binding)
-        render :text => 'OK'
-      end
-    # else
-      # render :status => 500
-    # end
+    if message.sender.match(/benjamin19890721@gmail.com/).nil?
+      Logger.rails.logger('message.sender is not benjamin19890721@gmail',
+        obj: message.sender, context: binding)
+    end
+
+    regex = /(http[s]?:\/\/youtu\.be\/.*)/
+    match = regex.match(message.plain_body)
+    if match.nil?
+      Rails.logger.debug!('No Match', context: binding)
+      render :text => 'No Match'
+    else
+      youtube_short_url = match[0]
+      youtube_long_url = ExpandUrlService.lengthen(youtube_short_url)
+      youtube_vidoe = YoutubeVideo.new(youtube_long_url)
+      ApplicationMailer.send_youtube_digest_to_evernote({
+        subject: 'Youtube Video URL',
+        youtube_vidoe: youtube_vidoe }
+        ).deliver_now
+      Rails.logger.debug!('OK', context: binding)
+      render :text => 'OK'
+    end
   end
 
 private
